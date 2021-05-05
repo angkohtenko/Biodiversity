c4a5a887287bb8b1ca67e1ed3dcb057f7af0ebc4
Angela <angkohtenko@gmail.com>
2021-05-05 12:14:07 -0400

    Top 5 city growths plotted

---
+++ plot.js
@@ -0,0 +1,24 @@
+// five largest cities by growth
+var sortedCities = cityGrowths.sort((a, b) => b.Increase_from_2016 - a.Increase_from_2016);
+var topFiveCities = sortedCities.slice(0,5);
+var topFiveCityNames = topFiveCities.map(city => city.City);
+var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+
+var trace ={
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+};
+
+var data = [trace];
+
+var layout ={
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City"},
+    yaxis: {title: "Population Growth, 2016-2017"}
+};
+
+Plotly.newPlot("bar-plot", data, layout);
+
+
+
