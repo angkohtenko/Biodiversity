5fa6c721a7f9eeae1d0740443ea5019abc9e64f4
Angela <angkohtenko@gmail.com>
2021-05-07 11:56:14 -0400

    Bar chart with logs

---
+++ charts.js
@@ -0,0 +1,98 @@
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+// Initialize the dashboard
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
+  
+}
+
+// Demographics Panel 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    console.log(data)
+    let metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    let PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
+
+// 1. Create the buildCharts function.
+function buildCharts(sample) {
+  // 2. Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    // 3. Create a variable that holds the samples array. 
+    let samples = data.samples;
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    //  5. Create a variable that holds the first sample in the array.
+    let result = resultArray[0];
+
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    let otu_ids = result["otu_ids"];
+    let otu_labels = result["otu_labels"];
+    let sample_values = result["sample_values"];
+    console.log(otu_ids)
+    console.log(otu_labels)
+    console.log(sample_values)
+
+    // 7. Create the yticks for the bar chart.
+  
+    var topTenValues = sample_values.sort((a, b) => b - a).slice(0, 10).reverse()
+    console.log(topTenValues)
+    var topTenIds = otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse()
+
+
+    // 8. Create the trace for the bar chart. 
+    var barData = [{
+      x: topTenValues,
+      y: topTenIds,
+      text: otu_labels,
+      type: "bar",
+      orientation: 'h'
+    }];
+    // 9. Create the layout for the bar chart. 
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      yaxis:{type: "category"}
+     
+    };
+    // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot('bar', barData, barLayout)
+  });
+}
